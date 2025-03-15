c78ec88b34c098e3413a43045d71b6bfa1385e01
Candra Sudirman <dev.cndrsdrmn@gmail.com>
2025-03-16 02:56:13 +0700

    feat: add method to override stack trace of exceptions

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,10 +5,32 @@ declare(strict_types=1);
 namespace Tests;
 
 use Cndrsdrmn\LaravelFailures\ServiceProvider;
+use Exception;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use ReflectionException;
+use ReflectionObject;
 
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * Overrides the stack trace of the given exception.
+     *
+     * @throws ReflectionException
+     */
+    final public function overrideStacktraceException(Exception &$exception, array $parameters = []): void
+    {
+        $reflection = new ReflectionObject($exception);
+
+        while ($reflection->getParentClass() !== false) {
+            $reflection = $reflection->getParentClass();
+        }
+
+        $traceReflection = $reflection->getProperty('trace');
+        $traceReflection->setAccessible(true);
+        $traceReflection->setValue($exception, $parameters);
+        $traceReflection->setAccessible(false);
+    }
+
     /**
      * Get package providers.
      *
