d95bf9893e8f423d0d6a95b394e35c0cc2d7bfaf
Candra Sudirman <dev.cndrsdrmn@gmail.com>
2025-03-16 01:49:29 +0700

    feat: add custom exception renderer registration and renderer retrieval methods

---
+++ src/FailureRenderer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cndrsdrmn\LaravelFailures;
+
+/**
+ * @internal
+ *
+ * @codeCoverageIgnore
+ */
+final class FailureRenderer
+{
+    /**
+     * Invokes the failure renderer.
+     */
+    public function __invoke(): void
+    {
+        //
+    }
+}
